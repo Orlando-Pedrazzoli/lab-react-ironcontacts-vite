5a66a82525d36e676457adfdc28cdb86fd957094
Orlando-Pedrazzoli <pedrazzoliorlando@gmail.com>
2023-11-25 15:40:43 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,83 @@
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+import contactsData from './contacts.json';
 
 function App() {
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+  const handleDelete = id => {
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+
+    setContacts(updatedContacts);
+  };
+
+  const addRandomContact = () => {
+    const remainingContacts = contactsData.filter(
+      contact => !contacts.some(c => c.id === contact.id)
+    );
+
+    if (remainingContacts.length > 0) {
+      const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+      const newContact = remainingContacts[randomIndex];
+      setContacts(prevContacts => [...prevContacts, newContact]);
+    }
+  };
+
+  const sortByName = () => {
+    const sortedContacts = [...contacts].sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    setContacts(sortedContacts);
+  };
+
+  const sortByPopularity = () => {
+    const sortedContacts = [...contacts].sort(
+      (a, b) => b.popularity - a.popularity
+    );
+    setContacts(sortedContacts);
+  };
+
   return (
-    <div className="App">
-      <h1>LAB | React IronContacts</h1>
+    <div className='App'>
+      <h1>Iron Contacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByName}>Sort by Name</button>
+      <button onClick={sortByPopularity}>Sort by Popularity</button>
+      <table className='table-container'>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Oscar</th>
+            <th>Emmy</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map(contact => (
+            <tr key={contact.id}>
+              <td>
+                <img
+                  className='contact-image'
+                  src={contact.pictureUrl}
+                  alt={`${contact.name} avatar`}
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar ? '🏆' : ''}</td>
+              <td>{contact.wonEmmy ? '🌟' : ''}</td>
+              <td>{contact.wonOscar ? '🏆' : ''}</td>
+              <td>{contact.wonEmmy ? '🌟' : ''}</td>
+              <td>
+                <button onClick={() => handleDelete(contact.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -1,66 +1,66 @@
-import { describe, test, expect } from "vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import App from "./../App";
-import contacts from "./../contacts.json";
+import { describe, test, expect } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './../App';
+import contacts from './../contacts.json';
 
-describe("Iteration 1 | Display 5 Contacts", () => {
-  describe("App component", () => {
-    test("renders a table", () => {
+describe('Iteration 1 | Display 5 Contacts', () => {
+  describe('App component', () => {
+    test('renders a table', () => {
       render(<App />);
-      const table = screen.getByRole("table");
+      const table = screen.getByRole('table');
       expect(table).toBeInTheDocument();
     });
 
-    test("renders a table showing first 5 contacts", () => {
+    test('renders a table showing first 5 contacts', () => {
       const { container } = render(<App />);
-      const tableRows = container.querySelectorAll("tbody tr");
+      const tableRows = container.querySelectorAll('tbody tr');
       expect(tableRows).toHaveLength(5); // 5 tr elements
     });
   });
 });
 
-describe("Iteration 2 | Conditionally Display Awards Info", () => {
-  describe("App component", () => {
-    test("renders Emmy and Oscar award table cells (<td></td>)", () => {
+describe('Iteration 2 | Conditionally Display Awards Info', () => {
+  describe('App component', () => {
+    test('renders Emmy and Oscar award table cells (<td></td>)', () => {
       const { container } = render(<App />);
-      const tableRows = container.querySelectorAll("tbody tr");
+      const tableRows = container.querySelectorAll('tbody tr');
 
-      Array.from(tableRows).forEach((row) => {
-        const oscarCell = row.querySelector("td:nth-child(4)");
-        const emmyCell = row.querySelector("td:nth-child(5)");
+      Array.from(tableRows).forEach(row => {
+        const oscarCell = row.querySelector('td:nth-child(4)');
+        const emmyCell = row.querySelector('td:nth-child(5)');
 
-        expect(oscarCell.tagName).toBe("TD");
-        expect(emmyCell.tagName).toBe("TD");
+        expect(oscarCell.tagName).toBe('TD');
+        expect(emmyCell.tagName).toBe('TD');
       });
     });
 
-    test("conditionally renders Oscar award emoji for each contact", () => {
+    test('conditionally renders Oscar award emoji for each contact', () => {
       const { container } = render(<App />);
-      const tableRows = container.querySelectorAll("tbody tr");
+      const tableRows = container.querySelectorAll('tbody tr');
 
       Array.from(tableRows).forEach((row, index) => {
         const hasOscar = contacts[index].wonOscar;
-        const oscarCell = row.querySelector("td:nth-child(4)");
+        const oscarCell = row.querySelector('td:nth-child(4)');
 
         if (hasOscar) {
-          expect(oscarCell).toHaveTextContent("🏆");
+          expect(oscarCell).toHaveTextContent('🏆');
         } else {
           expect(oscarCell).toBeEmptyDOMElement();
         }
       });
     });
 
-    test("conditionally renders Emmy award emoji for each contact", () => {
+    test('conditionally renders Emmy award emoji for each contact', () => {
       const { container } = render(<App />);
-      const tableRows = container.querySelectorAll("tbody tr");
+      const tableRows = container.querySelectorAll('tbody tr');
 
       Array.from(tableRows).forEach((row, index) => {
         const hasEmmy = contacts[index].wonEmmy;
-        const emmyCell = row.querySelector("td:nth-child(5)");
+        const emmyCell = row.querySelector('td:nth-child(5)');
 
         if (hasEmmy) {
-          expect(emmyCell).toHaveTextContent("🌟");
+          expect(emmyCell).toHaveTextContent('🌟');
         } else {
           expect(emmyCell).toBeEmptyDOMElement();
         }
@@ -69,11 +69,11 @@ describe("Iteration 2 | Conditionally Display Awards Info", () => {
   });
 });
 
-describe("Iteration 3 | Add New Random Contacts", () => {
-  describe("App component", () => {
+describe('Iteration 3 | Add New Random Contacts', () => {
+  describe('App component', () => {
     test("renders a 'Add Random Contact' button", () => {
       render(<App />);
-      const addButton = screen.getByRole("button", {
+      const addButton = screen.getByRole('button', {
         name: /Add Random Contact/i,
       });
       expect(addButton).toBeInTheDocument();
@@ -81,18 +81,18 @@ describe("Iteration 3 | Add New Random Contacts", () => {
 
     test("adds a random contact to the list when the 'Add Random Contact' button is clicked", () => {
       render(<App />);
-      const addButton = screen.getByRole("button", {
+      const addButton = screen.getByRole('button', {
         name: /Add Random Contact/i,
       });
       fireEvent.click(addButton);
-      const contactRows = screen.getAllByRole("row");
+      const contactRows = screen.getAllByRole('row');
       expect(contactRows).toHaveLength(7); // 6 contacts + header row
     });
 
     test("adds non-repeated random contacts when the 'Add Random Contact' button is clicked multiple times", () => {
       const { container } = render(<App />);
       const displayedContacts = contacts.slice(0, 5);
-      const addButton = screen.getByRole("button", {
+      const addButton = screen.getByRole('button', {
         name: /Add Random Contact/i,
       });
 
@@ -102,12 +102,12 @@ describe("Iteration 3 | Add New Random Contacts", () => {
         fireEvent.click(addButton);
       }
 
-      const tableRows = container.querySelectorAll("tbody tr");
+      const tableRows = container.querySelectorAll('tbody tr');
       const displayedContactNames = Array.from(tableRows).map(
-        (row) => row.querySelector("td:nth-child(2)").textContent
+        row => row.querySelector('td:nth-child(2)').textContent
       );
 
-      const hasDuplicates = (array) => new Set(array).size !== array.length;
+      const hasDuplicates = array => new Set(array).size !== array.length;
 
       expect(tableRows).toHaveLength(contactsLength); // contacts
       expect(hasDuplicates(displayedContactNames)).toBe(false);
@@ -115,11 +115,11 @@ describe("Iteration 3 | Add New Random Contacts", () => {
   });
 });
 
-describe("Iteration 4 | Sort Contacts by Name and Popularity", () => {
-  describe("App component", () => {
+describe('Iteration 4 | Sort Contacts by Name and Popularity', () => {
+  describe('App component', () => {
     test("renders a 'Sort by Name' button", () => {
       render(<App />);
-      const addButton = screen.getByRole("button", {
+      const addButton = screen.getByRole('button', {
         name: /Sort by Name/i,
       });
       expect(addButton).toBeInTheDocument();
@@ -127,7 +127,7 @@ describe("Iteration 4 | Sort Contacts by Name and Popularity", () => {
 
     test("renders a 'Sort by Popularity' button", () => {
       render(<App />);
-      const addButton = screen.getByRole("button", {
+      const addButton = screen.getByRole('button', {
         name: /Sort by Popularity/i,
       });
       expect(addButton).toBeInTheDocument();
@@ -135,7 +135,7 @@ describe("Iteration 4 | Sort Contacts by Name and Popularity", () => {
 
     test("sorts contacts by name when the 'Sort by Name' button is clicked", () => {
       render(<App />);
-      const sortByNameButton = screen.getByRole("button", {
+      const sortByNameButton = screen.getByRole('button', {
         name: /Sort by Name/i,
       });
       fireEvent.click(sortByNameButton);
@@ -145,7 +145,7 @@ describe("Iteration 4 | Sort Contacts by Name and Popularity", () => {
         .sort((a, b) => a.name.localeCompare(b.name));
 
       const nameCells = screen.getAllByText(/.*/, {
-        selector: "tbody tr td:nth-child(2)",
+        selector: 'tbody tr td:nth-child(2)',
       });
 
       sortedContactsByName.forEach((contact, index) => {
@@ -155,7 +155,7 @@ describe("Iteration 4 | Sort Contacts by Name and Popularity", () => {
 
     test("sorts contacts by popularity when the 'Sort by Popularity' button is clicked", () => {
       render(<App />);
-      const sortByPopularityButton = screen.getByRole("button", {
+      const sortByPopularityButton = screen.getByRole('button', {
         name: /Sort by Popularity/i,
       });
       fireEvent.click(sortByPopularityButton);
@@ -165,7 +165,7 @@ describe("Iteration 4 | Sort Contacts by Name and Popularity", () => {
         .sort((a, b) => b.popularity - a.popularity);
 
       const nameCells = screen.getAllByText(/.*/, {
-        selector: "tbody tr td:nth-child(2)",
+        selector: 'tbody tr td:nth-child(2)',
       });
 
       sortedContactsByPopularity.forEach((contact, index) => {
@@ -175,25 +175,25 @@ describe("Iteration 4 | Sort Contacts by Name and Popularity", () => {
   });
 });
 
-describe("Iteration 5 | Remove Contacts", () => {
-  describe("App component", () => {
+describe('Iteration 5 | Remove Contacts', () => {
+  describe('App component', () => {
     test("removes a specific contact from the list when the 'Delete' button is clicked", () => {
       const { container } = render(<App />);
-      const initialTableRows = container.querySelectorAll("tbody tr");
+      const initialTableRows = container.querySelectorAll('tbody tr');
       const randomRowIndex = Math.floor(
         Math.random() * initialTableRows.length
       );
       const contactNameToDelete =
         initialTableRows[randomRowIndex].querySelector(
-          "td:nth-child(2)"
+          'td:nth-child(2)'
         ).textContent;
 
-      const deleteButtons = screen.getAllByRole("button", { name: /Delete/i });
+      const deleteButtons = screen.getAllByRole('button', { name: /Delete/i });
       fireEvent.click(deleteButtons[randomRowIndex]);
 
-      const updatedTableRows = container.querySelectorAll("tbody tr");
+      const updatedTableRows = container.querySelectorAll('tbody tr');
       const updatedContactNames = Array.from(updatedTableRows).map(
-        (row) => row.querySelector("td:nth-child(2)").textContent
+        row => row.querySelector('td:nth-child(2)').textContent
       );
 
       expect(updatedTableRows).toHaveLength(initialTableRows.length - 1);
